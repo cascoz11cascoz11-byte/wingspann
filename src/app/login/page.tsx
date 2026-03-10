5901dae15fad3bde284c43510abe8e44d7a70aaf
cascoz11cascoz11-byte <cascoz11cascoz11@gmail.com>
2026-03-10 19:19:51 -0400

    fix apostrophe

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -46,7 +46,7 @@ export default function LoginPage() {
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-slate-500">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/signup" className="text-sky-600 hover:underline">Sign up</Link>
         </p>
       </div>
